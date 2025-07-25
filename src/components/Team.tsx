c1c308dd92af5ecdfd2a83a9d1d1b60c0797be47
vaibhav09981 <vaibhavjj99@gmail.com>
2025-07-25 21:16:54 +0530

    Error has been fixed

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -12,9 +12,7 @@ const Team = () => {
         </p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 pt-8 md:pt-16">
-        {TeamDetails.map((det, idx) => (
-          <TeamCard key={idx} {...det} />
-        ))}
+
       </div>
       <div className="sm:justify-self-end mt-10">
         <a href="#!" className="block">
@@ -27,49 +25,6 @@ const Team = () => {
   );
 };
 
-const TeamDetails: TeamCardProps[] = [
-  {
-    imageUrl: "/john.png",
-    name: "John Smith",
-    position: "CEO and Founder",
-    experience:
-      "10+ years of experience in digital marketing. Expertise in SEO, PPC, and content strategy",
-  },
-  {
-    imageUrl: "/jane.png",
-    name: "Jane Doe",
-    position: "Director of Operations",
-    experience:
-      "7+ years of experience in project management and team leadership. Strong organizational and communication skills",
-  },
-  {
-    imageUrl: "/michael.png",
-    name: "Michael Brown",
-    position: "Senior SEO Specialist",
-    experience:
-      "5+ years of experience in SEO and content creation. Proficient in keyword research and on-page optimization",
-  },
-  {
-    imageUrl: "/emily.png",
-    name: "Emily Johnson",
-    position: "PPC Manager",
-    experience:
-      "3+ years of experience in paid search advertising. Skilled in campaign management and performance analysis",
-  },
-  {
-    imageUrl: "/brian.png",
-    name: "Brian Williams",
-    position: "Social Media Specialist",
-    experience:
-      "4+ years of experience in social media marketing. Proficient in creating and scheduling content, analyzing metrics, and building engagement",
-  },
-  {
-    imageUrl: "/sarah.png",
-    name: "John Smith",
-    position: "CEO and Founder",
-    experience:
-      "2+ years of experience in writing and editing Skilled in creating compelling, SEO-optimized content for various industries",
-  },
-];
+
 
 export default Team;
